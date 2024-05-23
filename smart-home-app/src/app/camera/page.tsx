192f0e7fadf32fd8c7206e206f78b5e4b13e059a
Huy Nguyen Ngo <huy.nguyenngo13@hcmut.edu.vn>
2024-05-23 13:12:39 +0700

    Fix UI

--- smart-home-app/src/app/camera/page.tsx
+++ smart-home-app/src/app/camera/page.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@nextui-org/react"
+import Link from "next/link"
+
 export default function CameraPage() {
   return (
     <div>
       <p>Implement your page here.</p>
+      <Link href='1.1.1.1'> {/* Insert your link here */}
+        <Button>Button 1</Button>
+      </Link>
+      <Link href='1.1.2.2'> {/* Insert your link here */}
+        <Button>Button 2</Button>
+      </Link>
     </div>
   )
 }
